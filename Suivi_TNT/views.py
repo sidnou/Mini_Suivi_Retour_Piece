5c1c0885b9a7589b4448f3c15a20d63175d60683
j-asi <sidjack972@gmail.com>
2023-02-14 23:13:26 +0100

    Creation Application Web

--- Suivi_TNT/views.py
+++ Suivi_TNT/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+AUTEUR = "Jacques-a S."
+VERSION = "0.0.1beta"
+
+
+def index(request):
+    context = {
+        'Auteur': AUTEUR,
+        'Version': VERSION,
+        'Titre': 'Suivi TNT',
+    }
+    return render(request, 'Suivi_TNT/index.html', context)
