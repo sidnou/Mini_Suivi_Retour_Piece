21c510044c54392dba00e8aa2f91303f55fa36ec
jasid <sidjack972@gmail.com>
2023-02-20 13:33:04 +0100

    Création Application Web
    Rectification urls

--- Suivi_TNT/urls.py
+++ Suivi_TNT/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path("suivi-tnt/add/", views.add_suivi_tnt, name="add-suivi-tnt"),
+    path("add/", views.add_suivi_tnt, name="add-suivi-tnt"),
 
 ]
