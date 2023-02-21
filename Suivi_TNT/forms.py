5d23226e5c1cbce172c0bbad28d77e28a0ed7bcc
jasid <sidjack972@gmail.com>
2023-02-21 13:20:05 +0100

    Création Application Web
    mise en page de la vue Ajout Suivi tnt
    
    Petite modification de la base de donnée

--- Suivi_TNT/forms.py
+++ Suivi_TNT/forms.py
@@ -6,9 +6,16 @@ class SuiviTntForm(forms.ModelForm):
     class Meta:
         model = SuiviRetourPieceHP
         fields = "__all__"
+        labels = {
+            'reference': '',
+            'numero_commande': '',
+            'numero_dossier': '',
+            'numero_suivi': '',
+        }
         widgets = {
-            'reference': forms.TextInput(attrs={'class': 'form-control m-1'}),
-            'numero_commande': forms.TextInput(attrs={'class': 'form-control mb-1'}),
-            'numero_dossier': forms.TextInput(attrs={'class': 'form-control mb-1'}),
-            'numero_suivi': forms.TextInput(attrs={'class': 'form-control mb-1'})
+            'reference': forms.TextInput(attrs={'class': 'form-control mb-1', 'placeholder': 'Référence'}),
+            'numero_commande': forms.TextInput(
+                attrs={'class': 'form-control mb-1', 'placeholder': 'Numéro Commande'}),
+            'numero_dossier': forms.TextInput(attrs={'class': 'form-control mb-1', 'placeholder': 'Numéro Dossier'}),
+            'numero_suivi': forms.TextInput(attrs={'class': 'form-control mb-1', 'placeholder': 'Numèro Suivi TNT'})
         }
--- Suivi_TNT/models.py
+++ Suivi_TNT/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 
 class SuiviRetourPieceHP(models.Model):
-    reference = models.CharField(max_length=25)
-    numero_commande = models.CharField(max_length=25, null=False)
+    reference = models.CharField(max_length=10)
+    numero_commande = models.CharField(max_length=10, null=False)
     numero_dossier = models.CharField(max_length=15, null=False)
-    numero_suivi = models.CharField(max_length=25, unique=True, null=False)
+    numero_suivi = models.CharField(max_length=20, unique=True, null=False)
     date_creation = models.DateField(auto_now_add=True)
 
     def __str__(self):
